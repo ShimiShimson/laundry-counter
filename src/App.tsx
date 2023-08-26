6901457cff66dbc31ac95f5a39d99e71d23f3a1f
Shimi Shimson <szymon.jakub.adamczuk@gmail.com>
2023-08-26 21:14:43 +0200

    - rewritten styles - added input for modifying number of laundry counts without buttons - added password check

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,59 @@
-import './App.css'
-import { useFirebaseService } from './firebase/useFirebase.service';
-
+import "./App.css";
+import { useFirebaseService } from "./firebase/useFirebase.service";
 
 function App() {
+
+  const { laundryCounts, handleDecrement, handleIncrement, handleInputChange } =
+    useFirebaseService();
   
-  const {
-    laundryCounts,
-    handleDecrement,
-    handleIncrement
-  } = useFirebaseService();
-
-
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Imię</th>
-          <th>Ilość prań</th>
-          <th>Akcje</th>
-        </tr>
-      </thead>
-      <tbody>
-        {laundryCounts && laundryCounts.map(({name, number}) => (
-          <tr key={name}>
-            <td>{name}</td>
-            <td>{number}</td>
-            <td>
-              <button onClick={() => handleDecrement(name)}>-</button>
-              <button onClick={() => handleIncrement(name)}>+</button>
-            </td>
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Imię</th>
+            <th>Ilość prań</th>
+            <th>Akcje</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  )
+        </thead>
+        <tbody>
+          {laundryCounts &&
+            laundryCounts.map(({ name, number }) => (
+              <tr key={name}>
+                <td>{name}</td>
+                <td>{number}</td>
+                <td>
+                  <button
+                    className="decrement-button"
+                    onClick={() => handleDecrement(name)}
+                  >
+                    -
+                  </button>
+                  <button
+                    className="increment-button"
+                    onClick={() => handleIncrement(name)}
+                  >
+                    +
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      <div className="input-column">
+        {laundryCounts &&
+          laundryCounts.map(({ name }) => (
+            <div key={name} className="input-row">
+              <span>{name}</span>
+              <input
+                type="number"
+                onChange={(e) => handleInputChange(name, e.target.value)}
+              />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
